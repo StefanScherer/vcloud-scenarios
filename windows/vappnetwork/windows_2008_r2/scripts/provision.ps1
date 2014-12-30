9698099725406691d463488733785a2cc4fa209d
Stefan Scherer <scherer_stefan@icloud.com>
2014-12-30 11:00:09 +0100

    played with vapp_name and add_hdds keys

--- windows/vappnetwork/windows_2008_r2/scripts/provision.ps1
+++ windows/vappnetwork/windows_2008_r2/scripts/provision.ps1
@@ -4,4 +4,18 @@ $box = $box.ComputerName.ToString().ToLower()
 Write-Host "This guest box is $box (registry ComputerName)"
 Write-Host "This guest box is $env:COMPUTERNAME (env COMPUTERNAME)"
 
+Write-Host "Adding second disk D"
+
+$addDriveD = @'
+select disk 1
+attributes disk clear readonly
+online disk
+create partition primary
+format quick
+assign letter D
+'@
+
+$addDriveD | Out-File $env:TEMP\addDriveD.txt -Encoding ascii
+& diskpart /s $env:TEMP\addDriveD.txt
+
 Write-Host "Provisioning done"
